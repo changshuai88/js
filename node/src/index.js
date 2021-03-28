5ef5f49ad138817bdc5e1fcc1cf0ff8bd685aa88
changshuai <2233282817@qq.com>
2021-03-28 10:18:06 +0800

    20210328webpack打包

---
+++ node/src/index.js
@@ -0,0 +1,2 @@
+import Style from "./style.js";
+new Style().init();
---
+++ node/src/style.js
@@ -0,0 +1,9 @@
+// const show=function (){
+//         return document.body.style.backgroundColor="red";
+// }
+export default class Style{
+    constructor(){}
+    init(){
+        document.body.style.backgroundColor="yellow";
+    }
+}
