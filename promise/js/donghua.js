44e4077c33f92e7865f01ef84ae143ce285b3be5
changshuai <2233282817@qq.com>
2021-05-06 21:36:29 +0800

    链式操作

---
+++ promise/js/donghua.js
@@ -0,0 +1,25 @@
+function interval(delay=1000,callback){
+    return new Promise(resolve=>{
+        let id=setInterval(() => {
+            callback(id,resolve);
+        }, delay);
+    });
+}
+
+interval(100,(id,resolve)=>{
+    const div=document.querySelector("div");
+    let left=parseInt(window.getComputedStyle(div).left);
+    div.style.left=left+10+"px";
+    if(left>=200){
+        clearInterval(id);
+        resolve(div);
+    }
+}).then(div=>{
+    interval(50,(id,resolve)=>{
+        let width=parseInt(window.getComputedStyle(div).width);
+        div.style.width=width-10+"px";
+        if (width<=20) {
+            clearInterval(id);
+        }
+    });
+});
